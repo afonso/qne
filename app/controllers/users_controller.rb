22b011bbbf5f82e03965c7369d6e6f25dc10341b
afonso <afonso@yack.com.br>
2016-04-27 01:15:07 -0300

    novas escolas

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,10 @@ class UsersController < ApplicationController
     @groups = Group.where(user_id: current_user.id)
     @proposals = Proposal.where(user_id: current_user.id)
     @volunteereds = Demand.where(accepted_by: current_user.id)
-    if @demands.size == 0 and @groups.size == 0
+    if @demands.size == 0 and @groups.size == 0 and current_user.role == 'student'
       flash[:notice] = 'Pronto! Veja os pedidos de outras pessoas ou crie uma clicando no botão abaixo.'
       redirect_to demands_path
-    end
-    if @proposals.size == 0 and @volunteereds.size == 0
+    elsif @proposals.size == 0 and @volunteereds.size == 0 and current_user.role != 'student'
       flash[:notice] = 'Pronto! Escolha um pedido para ajudar'
       redirect_to demands_path
     end
